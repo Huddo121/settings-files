18dc37f26dda86717cfebc345452459f527554d4
Michael Hudson <huddo121@gmail.com>
2018-07-10 15:30:15 +1000

    Added the starts of a syncing shell script

---
+++ sync-config.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+__FILES="
+	i3/config
+	polybar/config
+	polybar/launch.sh
+	kitty/kitty.conf
+	compton.conf"
+
+files=($__FILES)
+
+# I'm sure I could do something here that would smartly copy files back and forth
